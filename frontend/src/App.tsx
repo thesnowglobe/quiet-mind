49a60fc64e8bb198617f42818dbbf6c53236aaa1
m0chi/ <aly0mal99@gmail.com>
2024-08-09 13:22:15 +0100

    basic idea of routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,22 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Header from "./blocks/Header"
+import Main from "./blocks/Main"
+import Footer from "./blocks/Footer"
+import SignUp from "./blocks/SignUp"
+import SignIn from "./blocks/SignIn"
 
 function App() {
   
   return (
-    <div></div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
