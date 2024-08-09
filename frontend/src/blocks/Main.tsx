619b656592c118dcddcba0d09a835a91c1acba8a
m0chi/ <aly0mal99@gmail.com>
2024-08-09 14:46:14 +0100

    structure + css

--- frontend/src/blocks/Main.tsx
+++ frontend/src/blocks/Main.tsx
@@ -1,7 +1,18 @@
+import chamomilebig from "../assets/chamomilebig.png"
+
 function Main() {
   return (
-    <div>
-
+    <div className="gridmain max-w-6xl m-auto">
+      <h1 className="flex flex-col text-[160px] uppercase">
+        <span className="self-center">Quiet</span>
+        <span className="self-center">Mind</span>
+      </h1>
+      <img src={chamomilebig} className="max-w-[400px] self-center justify-self-center"/>
+      <div>
+        <p>let your mind rest</p>
+        <p>dlfhdshfosdihfdosifhsoidfhsoidhfso</p>
+      </div>
+      
     </div>
   )
 }
